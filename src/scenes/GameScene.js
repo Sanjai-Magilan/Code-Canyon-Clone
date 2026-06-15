b2ee41e73529aeb5b4ae8665893fe8ca6d421b3c
Sanjai-Magilan <sanjai6115@gmail.com>
2026-06-15 20:47:49 +0530

    stons and bushes done

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,6 +5,8 @@ import wormRun from "../assets/Sprites/Enemy/run/worm_run.png";
 import shadowImg from "../assets/Sprites/Enemy/worm-shadow.png";
 import bg from "../assets/Sprites/BG/floor/tiledfloor.png";
 import farm from "../assets/Sprites/BG/farm/carrot.png";
+import decor1 from "../assets/Sprites/BG/stons/bgdesign-animation 1-000.png";
+import decor2 from "../assets/Sprites/BG/stons/bgdesign-animation 1-001.png";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -17,6 +19,9 @@ export default class GameScene extends Phaser.Scene {
     });
     this.load.image("worm-shadow", shadowImg);
     this.load.image("bg", bg);
+    this.load.image("carrot", farm);
+    this.load.image("decor1", decor1);
+    this.load.image("decor2", decor2);
   }
   create() {
     this.playerHP = 100;
@@ -29,6 +34,28 @@ export default class GameScene extends Phaser.Scene {
     this.bg.setDisplaySize(1920, 1080);
 
     this.bg.setDepth(-100);
+    this.decorations = this.add.group();
+    const decorPositions = [
+      // Top row
+      { x: 200, y: -50, key: "decor1" },
+      { x: 600, y: -50, key: "decor2" },
+      { x: 1050, y: -50, key: "decor1" },
+      { x: 1450, y: -50, key: "decor2" },
+      // { x: 1750, y: -50, key: "decor1" },
+
+      // Right side
+      { x: 1860, y: 150, key: "decor2" },
+      { x: 1880, y: 500, key: "decor1" },
+      { x: 1850, y: 950, key: "decor2" },
+    ];
+
+    decorPositions.forEach((pos) => {
+      const d = this.add.image(pos.x, pos.y, pos.key);
+
+      d.setDepth(-50);
+
+      d.setScale(0.8);
+    });
     // this.add.rectangle(960, 540, 1920, 1080).setStrokeStyle(10, 0xff0000);
     this.player = new Player(this, 150, 360);
     this.anims.create({
