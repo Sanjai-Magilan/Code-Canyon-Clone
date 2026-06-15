6c20ed60a01281dc71f4191d66cad6c9a3c4368b
Sanjai-Magilan <sanjai6115@gmail.com>
2026-06-15 19:21:32 +0530

    bg done partially

--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
 import Phaser from "phaser";
 import GameScene from "./scenes/GameScene";
-
+import "./style.css";
 const config = {
   type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+
+    width: 1920,
+    height: 1080,
+  },
   backgroundColor: "#222",
 
   physics: {
     default: "arcade",
     arcade: {
-      debug: false
-    }
+      debug: false,
+    },
   },
 
-  scene: [GameScene]
+  scene: [GameScene],
 };
 
 new Phaser.Game(config);
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,6 +3,9 @@ import Player from "../entities/Player";
 import Enemy from "../entities/Enemy";
 import wormRun from "../assets/Sprites/Enemy/run/worm_run.png";
 import shadowImg from "../assets/Sprites/Enemy/worm-shadow.png";
+import bg from "../assets/Sprites/BG/floor/tiledfloor.png";
+import farm from "../assets/Sprites/BG/farm/carrot.png";
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("GameScene");
@@ -13,10 +16,20 @@ export default class GameScene extends Phaser.Scene {
       frameHeight: 186,
     });
     this.load.image("worm-shadow", shadowImg);
+    this.load.image("bg", bg);
   }
   create() {
-    this.add.text(20, 20, "Farm Defense Clone", { fontSize: "32px" });
     this.playerHP = 100;
+    this.bg = this.add.image(
+      960, // 1920 / 2
+      540, // 1080 / 2
+      "bg",
+    );
+
+    this.bg.setDisplaySize(1920, 1080);
+
+    this.bg.setDepth(-100);
+    // this.add.rectangle(960, 540, 1920, 1080).setStrokeStyle(10, 0xff0000);
     this.player = new Player(this, 150, 360);
     this.anims.create({
       key: "worm-run",
