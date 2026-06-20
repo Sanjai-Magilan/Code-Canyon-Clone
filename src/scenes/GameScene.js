362849254c537de2b4924767912ecfd9279aeec3
Sanjai-Magilan <sanjai6115@gmail.com>
2026-06-21 01:24:19 +0530

    removed the electrode and wires

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -8,10 +8,7 @@ import farm from "../assets/Sprites/BG/farm/carrot.png";
 import decor1 from "../assets/Sprites/BG/stons/bgdesign-animation 1-000.png";
 import decor2 from "../assets/Sprites/BG/stons/bgdesign-animation 1-001.png";
 import floorPatch from "../assets/Sprites/BG/floor/popfade.png";
-import top_electrode from "../assets/Sprites/BG/electrode/electrode-a0-001.png";
-import bottom_electrode from "../assets/Sprites/BG/electrode/electrode-a0-000.png";
-import top_electrode_wire from "../assets/Sprites/BG/electrode/electrocable-animation 1-000.png";
-import bottom_electrode_wire from "../assets/Sprites/BG/electrode/electrocable-animation 1-001.png";
+
 import playerHead from "../assets/Sprites/Player/head/playerhead-default-000.png";
 import playerRun from "../assets/Sprites/Player/run/player_run.png";
 import playerGun from "../assets/Sprites/Guns/gun0/playergun-gun-000.png";
@@ -32,10 +29,7 @@ export default class GameScene extends Phaser.Scene {
     this.load.image("decor1", decor1);
     this.load.image("decor2", decor2);
     this.load.image("floorPatch", floorPatch);
-    this.load.image("topElectrode", top_electrode);
-    this.load.image("bottomElectrode", bottom_electrode);
-    this.load.image("topElectrodeWire", top_electrode_wire);
-    this.load.image("bottomElectrodeWire", bottom_electrode_wire);
+
     this.load.spritesheet("player", playerRun, {
       frameWidth: 80,
       frameHeight: 103,
@@ -120,17 +114,7 @@ export default class GameScene extends Phaser.Scene {
         this.enemy.moveOneBeat();
       },
     });
-    // Top electrode
-    this.add.image(350, 220, "topElectrode").setScale(0.8).setDepth(10);
 
-    // Bottom electrode
-    this.add.image(350, 950, "bottomElectrode").setScale(0.8).setDepth(10);
-
-    // Top electrode wire
-    this.add.image(145, 240, "topElectrodeWire").setScale(0.8).setDepth(9);
-
-    // Bottom electrode wire
-    this.add.image(145, 1020, "bottomElectrodeWire").setScale(0.8).setDepth(9);
   }
   update() {
     this.player.update(this.cursors);
