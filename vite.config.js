55e58421e219dfeb3ab056c419b1d7c5b5c881d7
Sanjai-Magilan <sanjai6115@gmail.com>
2026-06-21 02:25:08 +0530

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Set relative base path so resources are loaded relatively
+  base: './',
+  build: {
+    // Inline all assets as base64 data URLs if they are under 10MB.
+    // This allows the build to run directly on the file:// protocol (e.g. double clicking index.html)
+    // without encountering browser Same-Origin Policy (CORS) errors.
+    assetsInlineLimit: 5000000,
+  },
+});
